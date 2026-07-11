55b8a5bc9073eb2cbcf51e6d7eb43370df159bfa
Harshil015 <118798133+Harshil015@users.noreply.github.com>
2026-07-11 15:03:23 +0530

    Update setup.sh

--- infra/setup.sh
+++ infra/setup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+echo "[*] Spinning up CTF Docker environment..."
+cd infra/
+
+# Build and run in detached mode
+docker-compose up -d --build
+
+echo "[*] Dropping into CTF shell..."
+# Execute bash inside the running container
+docker exec -it ctf_toolkit bash
+
+echo "[*] Exited container."
+echo "[*] The container is still running in the background."
+echo "[*] To stop it completely: cd infra/ && docker-compose down"
