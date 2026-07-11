2c97fe6739de13320aa1d62e2a2f15b9071984d3
Harshil015 <118798133+Harshil015@users.noreply.github.com>
2026-07-11 14:57:35 +0530

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,15 @@
+from colorama import Fore, Style, init
 
+# Initialize colorama
+init(autoreset=True)
+
+def print_banner():
+    banner = r"""
+     ____ _____ ____   ____ _   _ ___ _   _ _____ ____
+    / ___|_   _|  _ \ / ___| | | |_ _| \ | | ____/ ___|
+    \___ \ | | | |_) | |   | |_| || ||  \| |  _| \___ \
+     ___) || | |  _ <| |___|  _  || || |\  | |___ ___) |
+    |____/ |_| |_| \_\\____|_| |_|___|_| \_|_____|____/
+    """
+    print(f"{Fore.BLUE}{banner}{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}[+] CTF Automation Toolkit{Style.RESET_ALL}\n")
